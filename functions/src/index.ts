38b9c53821a4c65b64990d15e285d122153b2746
Goryudyuma <umaretekyoumade@gmail.com>
2019-02-04 23:57:32 +0900

    functionsを使って、userが初めてログインした時と、退会時の処理を書いた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,11 @@
-//import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +13,15 @@
 //export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 //});
+
+export const firstLogin = functions.auth.user().onCreate(user => {
+  return db.collection("users")
+    .doc(user.uid.toString())
+    .set({ labonow: false });
+});
+
+export const deleteUser = functions.auth.user().onDelete(user => {
+  return db.collection("users")
+    .doc(user.uid.toString())
+    .delete();
+});
