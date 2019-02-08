86498a22ef60f73316e12da538907394494d59f1
Goryudyuma <umaretekyoumade@gmail.com>
2019-02-09 05:23:53 +0900

    ついでに色々有効にしてみた

--- frontpage/src/index.js
+++ frontpage/src/index.js
@@ -137,15 +137,15 @@ firebase.auth().onAuthStateChanged(function(user) {
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID
-        //    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        //    firebase.auth.EmailAuthProvider.PROVIDER_ID
-        //    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
       // Terms of service url.
-      tosUrl: "<your-tos-url>",
+      tosUrl: "/",
       // Privacy policy url.
-      privacyPolicyUrl: "labotter2.063.jp"
+      privacyPolicyUrl: "/"
     };
 
     // The start method will wait until the DOM is loaded.
